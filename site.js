d825fc960e6be20c96e0ad63192f0564c1ab2074
trviethoang <46689993+trviethoang@users.noreply.github.com>
2019-01-22 00:51:51 +0700

    Update site.js

--- site.js
+++ site.js
@@ -4,16 +4,12 @@ document.getElementById('syn').addEventListener('click', function(){
 	var f=document.getElementById('f').value;
 	var zo=document.getElementById('zo').value;
 	var EL=document.getElementById('EL').value;
-	var h_u=document.getElementById('h_unit').value;
 
 	var A=zo*Math.sqrt(er/2+0.5)/60+(er-1)*(0.23+(0.11/er))/(er-(0-1));
 	var B=377*Math.PI/(2*zo*Math.sqrt(er));
-	console.log(B);
 	var W=8*h*Math.pow(Math.E, A)/(Math.pow(Math.E, 2*A)-2);
 	var ee=(er-(0-1))/2+(er-1)/(2*Math.sqrt(12*h/W-(0-1)));
-	console.log(ee);
 	var ko=20000000000*Math.PI*f/2997924580000;
-	console.log(ko);
 	var L=EL*Math.PI/(180*(Math.sqrt(ee)*ko));
 
 	document.getElementById('W').value=W;
@@ -26,12 +22,18 @@ document.getElementById('anal').addEventListener('click', function(){
 	var f=document.getElementById('f').value;
 	var W=document.getElementById('W').value;
 	var L=document.getElementById('L').value;
-	var h_u=document.getElementById('h_unit').value;
-	var W_u=document.getElementById('W_unit').value;
-	var L_u=document.getElementById('L_unit').value;
 
-	var zo=1;
-	var EL=1;
+	var ee=(er-(0-1))/2+(er-1)/(2*Math.sqrt(12*h/W-(0-1)));
+	var ko=20000000000*Math.PI*f/2997924580000;
+	var zo=0;
+	if(W/h<=1) {
+		zo=Math.log(8*h/W+W/4/h)*60/Math.sqrt(ee);
+	}
+	else {
+		zo=120*Math.PI/Math.sqrt(ee)/(W/h-(0-1.393)-(0-0.667*Math.log(W/d+1.444)));
+	}
+	
+	var EL=L*Math.sqrt(ee)*ko*180/Math.PI;
 
 	document.getElementById('zo').value=zo;
 	document.getElementById('EL').value=EL;
